756fd887f9bc53a258d2fd842c5403c410d1b95a
Farhan <farhan@aththariq.com>
2024-05-31 16:42:53 +0700

    chore: Disable consistent type imports rule in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
   rules: {
     'vue/first-attribute-linebreak': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
   },
 });
 // Your custom configs here
