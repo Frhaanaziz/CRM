7275faf63f2b430d853e612cae2d666dd2093718
Farhan <farhan@aththariq.com>
2024-06-03 20:43:10 +0700

    feat: Add dashboard companies page and sidebar component

---
+++ pages/dashboard/companies/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'dashboard'
+});
+
+//     Be careful, if you want to call this route on SSR, you must send your browser cookies including your supabase token.
+// const { data: { libraries }} = await useFetch('/api/libraries', {
+//   headers: useRequestHeaders(['cookie'])
+// })
+</script>
+
+
+
+<template>
+    <div class="px-4 sm:px-6 py-8">
+        <h1 class="text-2xl font-semibold">
+            B2B Company Database
+        </h1>
+
+        <div class="mt-10 grid grid-cols-3 gap-5">
+            <div class="p-3 border border-default rounded-md">
+                <p class="text-4xl text-brand font-semibold">60</p>
+                <p>Companies</p>
+            </div>
+            <div class="p-3 border border-default rounded-md">
+                <p class="text-4xl text-brand font-semibold">25</p>
+                <p>Industries</p>
+            </div>
+            <div class="p-3 border border-default rounded-md">
+                <p class="text-4xl text-brand font-semibold">30</p>
+                <p>Locations</p>
+            </div>
+        </div>
+    </div>
+
+</template>
