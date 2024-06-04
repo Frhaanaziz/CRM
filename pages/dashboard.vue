e38e203b1b16d419809a6df002f14355aff0e5b9
Farhan <farhan@aththariq.com>
2024-06-04 10:27:20 +0700

    feat: Add kinde auth

---
+++ pages/dashboard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'dashboard',
+    middleware: [
+        'auth-logged-in'
+    ],
+});
+</script>
+
+<template>
+    <NuxtPage />
+</template>
