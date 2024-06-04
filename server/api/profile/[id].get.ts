78da26cda90bddd03cfbfd711e6794aca6101242
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-04 16:20:16 +0800

    feat: Add profile APIs

---
+++ server/api/profile/[id].get.ts
@@ -0,0 +1,16 @@
+import { H3Event } from 'h3'
+import { serverSupabaseClient } from '#supabase/server'
+
+export default defineEventHandler(async (event: H3Event) => {
+  const supabase = await serverSupabaseClient(event)
+
+  // get user id from params
+  const id = event.context.params?.id
+  if (!id) throw createError({ status: 400, statusMessage: 'User id is needed' })
+
+  // get user from supabase
+  const { data: user, error } = await supabase.from('Users').select('*').eq('id', id).single()
+  if (error) throw createError({ status: 500, statusMessage: error.message })
+
+  return { data: user }
+})
