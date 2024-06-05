bcb3b1c85ea875c7e6c7cfcebe35270fbac7c954
Farhan <farhan@aththariq.com>
2024-06-05 12:59:18 +0700

    feat: Add count APIs for Companies, Industries, and Locations

---
+++ server/api/locations/count.get.ts
@@ -0,0 +1,20 @@
+import { H3Event } from 'h3';
+import type { Database } from '~/types/supabase';
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event: H3Event) => {
+    const supabase = await serverSupabaseClient<Database>(event);
+
+    const countRes = await supabase
+        .from('Cities')
+        .select('*', { count: 'exact', head: true });
+    if (countRes.error) {
+        console.error('Failed to get Cities count', countRes.error);
+        throw createError({
+            status: 500,
+            statusMessage: 'Failed to get Cities count',
+        });
+    }
+
+    return countRes.count;
+});
