e24597ce71d8cb878fff6289cf2b00136e7d068f
Farhan <farhan@aththariq.com>
2024-06-05 12:58:54 +0700

    chore: Disable explicit any type in TypeScript linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ export default withNuxt({
     'vue/first-attribute-linebreak': 'off',
     '@typescript-eslint/consistent-type-imports': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 });
 // Your custom configs here
