d207e4506ec4169e56296ade01f43e276b81b1fd
Farhan <farhan@aththariq.com>
2024-06-05 17:31:09 +0700

    chore: Disable self-closing HTML tags in Vue templates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ export default withNuxt({
     '@typescript-eslint/consistent-type-imports': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'off',
+    'vue/html-self-closing': 'off',
   },
 });
 // Your custom configs here
