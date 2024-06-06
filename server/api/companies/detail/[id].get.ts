e40d6c5c585a14ea4db582bb5437e9f294976ae4
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-06 13:13:01 +0800

    feat: Add company detail API

---
+++ server/api/companies/detail/[id].get.ts
@@ -0,0 +1,25 @@
+import { H3Event } from 'h3'
+import { serverSupabaseClient } from '#supabase/server'
+
+export default defineEventHandler(async (event: H3Event) => {
+  const supabase = await serverSupabaseClient(event)
+
+  // get user id from params
+  const id = event.context.params?.id
+  if (!id) throw createError({ status: 400, statusMessage: 'Company id is needed' })
+
+  // get company details from supabase
+  const { data: company, error } = await supabase.from('Companies').select(
+    `
+      *,
+      industry: Industries(name),
+      size: Sizes(size_range),
+      province: Provinces(name),
+      city: Cities(name),
+      photos: Photos(file)
+    `
+  ).eq('id', id).single()
+  if (error) throw createError({ status: 500, statusMessage: error.message })
+
+  return { data: company }
+})
