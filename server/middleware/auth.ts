fe0d324c3a794de92751fd93b7fdc128545bc73e
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2024-06-10 11:56:18 +0800

    feat: Update auth server middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,13 +1,27 @@
 import { H3Event } from 'h3'
-import { useAuth } from '~/.nuxt/imports'
+import { serverSupabaseClient } from '#supabase/server'
+import jwt from 'jsonwebtoken'
 
 export default defineEventHandler(async (event: H3Event) => {
-  // // allow public access to auth routes
-  // if (event.context.path.startsWith('/api/auth')) return
-  //
-  // // check user session
-  // const user = useAuth()
-  // if (!user.loggedIn) throw createError({ status: 401, statusMessage: 'Unauthorized' })
-  //
-  // return
+  const supabase = await serverSupabaseClient(event)
+
+  // allow public access to auth routes
+  if (event.path.startsWith('/api/auth')) return
+
+  // check headers for user
+  const token = event.headers.get('Authorization')?.split(' ')[1]
+  if (!token) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+
+  await jwt.verify(token, process.env.JWT_SECRET!, async (err: any, decoded: any) => {
+    if (err) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+
+    // get user id from decoded token
+    const userId = decoded.id
+
+    // get user from supabase
+    const { error } = await supabase.from('Users').select().eq('id', userId).single()
+    if (error) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+  })
+
+  return
 })
