b56cd502e04467da04fc47e9bdcb9d8a0652ab0e
Farhan <farhan@aththariq.com>
2024-06-10 19:24:36 +0700

    refactor: Add error handling to error.vue component

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+    error: {
+        type: Object as () => NuxtError,
+        default: null,
+    },
+});
+
+if (props.error && props.error.statusCode === 401)
+    clearError({ redirect: '/auth/signin' });
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<template>
+    <div>
+        <h2>{{ error?.statusCode }}</h2>
+        <button @click="handleError">Clear errors</button>
+    </div>
+</template>
