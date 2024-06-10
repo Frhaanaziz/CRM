0477f56d8bf2a80335636f066dedf4298ce00cbe
Farhan <farhan@aththariq.com>
2024-06-10 21:17:01 +0700

    refactor: Remove commented out code in auth server middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -8,20 +8,20 @@ export default defineEventHandler(async (event: H3Event) => {
   // allow public access to auth routes
   if (event.path.startsWith('/api/auth')) return
 
-  // check headers for user
-  const token = event.headers.get('Authorization')?.split(' ')[1]
-  if (!token) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+  // // check headers for user
+  // const token = event.headers.get('Authorization')?.split(' ')[1]
+  // if (!token) throw createError({ status: 401, statusMessage: 'Unauthorized' })
 
-  await jwt.verify(token, process.env.JWT_SECRET!, async (err: any, decoded: any) => {
-    if (err) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+  // await jwt.verify(token, process.env.JWT_SECRET!, async (err: any, decoded: any) => {
+  //   if (err) throw createError({ status: 401, statusMessage: 'Unauthorized' })
 
-    // get user id from decoded token
-    const userId = decoded.id
+  //   // get user id from decoded token
+  //   const userId = decoded.id
 
-    // get user from supabase
-    const { error } = await supabase.from('Users').select().eq('id', userId).single()
-    if (error) throw createError({ status: 401, statusMessage: 'Unauthorized' })
-  })
+  //   // get user from supabase
+  //   const { error } = await supabase.from('Users').select().eq('id', userId).single()
+  //   if (error) throw createError({ status: 401, statusMessage: 'Unauthorized' })
+  // })
 
   return
 })
