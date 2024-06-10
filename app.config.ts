b2ef84ab45e3a2765be37b22e3e2771851fc579e
Farhan <farhan@aththariq.com>
2024-06-10 19:22:25 +0700

    fix: Fix type-checking error in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,18 @@
 export default defineAppConfig({
-  ui: {
-    // Set the default theme to 'brand'
-    primary: 'brand',
+    ui: {
+        // Set the default theme to 'brand'
+        primary: 'brand',
 
-    notifications: {
-      // Show toasts at the top right of the screen
-      position: 'top-0 bottom-auto',
+        notifications: {
+            // Show toasts at the top right of the screen
+            position: 'top-0 bottom-auto',
+        },
+
+        input: {
+            base: 'disabled:bg-base-300',
+        },
     },
 
-    input: {
-      base: 'disabled:bg-base-300'
-    }
-  },
+    // fix type-checkin error
+    nuxtIcon: {},
 });
