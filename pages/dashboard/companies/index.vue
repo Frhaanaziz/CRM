692a0b7c101ab54423e94330c7ec09b34dd096b6
Farhan <farhan@aththariq.com>
2024-06-12 17:33:26 +0700

    feat: Remove commented out code in companies index.vue file

--- pages/dashboard/companies/index.vue
+++ pages/dashboard/companies/index.vue
@@ -13,10 +13,6 @@ const { data: dataSummary } = await useAsyncData(async () => {
 <template>
     <div class="px-4 sm:px-6 py-8">
         <h1 class="text-2xl font-semibold">B2B Company Database</h1>
-        <!-- <pre class="text-2xl font-semibold">
-            {{ $auth.user }}
-        </pre>
-        <pre class="text-2xl font-semibold mt-20">{{ $config }} </pre> -->
         <div class="mt-10 grid grid-cols-3 gap-5">
             <div
                 v-for="data in dataSummary"
