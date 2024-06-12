f2468b2b916eeca84b725e35d192982b4148bbdc
Farhan <farhan@aththariq.com>
2024-06-12 17:34:33 +0700

    feat(companies): Add sorting, and searching functionality to companies table

--- components/CompaniesTable.vue
+++ components/CompaniesTable.vue
@@ -1,14 +1,155 @@
-<script setup lang="ts">
-import { useDebounce } from '@vueuse/core';
+<script lang="ts" setup>
+import { useDateFormat } from '@vueuse/core';
+import type { City, Company, Industry, Province, Size } from '~/types';
 
-// Unable to use custom useAPI due to potential type error in the transform function
-const { data: companies, pending } = await useLazyFetch(
-    '/api/companies',
+interface IDataCompany extends Company {
+    industry: Pick<Industry, 'name'>;
+    size: Pick<Size, 'size_range'>;
+    province: Pick<Province, 'name'>;
+    city: Pick<City, 'name'>;
+}
+
+interface ITableCompany
+    extends Pick<Company, 'id' | 'name' | 'website' | 'avatar'> {
+    industry: string;
+    size: string;
+    location: string;
+}
+
+interface FilterOptions {
+    search: string;
+    page: number;
+    limit: number;
+    sort: string;
+    order: 'asc' | 'desc';
+}
+
+function filterAndPaginate(
+    data: ITableCompany[],
+    options: FilterOptions
+): ITableCompany[] {
+    // Filter by search term
+    const filteredData = data.filter((person) =>
+        person.name.toLowerCase().includes(options.search.toLowerCase())
+    );
+
+    // Sort data
+    const sortedData = filteredData.sort((a, b) => {
+        const sortField = options.sort as keyof ITableCompany;
+        const aValue = a[sortField];
+        const bValue = b[sortField];
+        // Skip sorting if the value is null or undefined
+        if (!aValue || !bValue) return 0;
+
+        if (aValue < bValue) return options.order === 'asc' ? -1 : 1;
+        if (aValue > bValue) return options.order === 'asc' ? 1 : -1;
+        return 0;
+    });
+
+    // Paginate data
+    const startIndex = (options.page - 1) * options.limit;
+    const endIndex = startIndex + options.limit;
+    const paginatedData = sortedData.slice(startIndex, endIndex);
+
+    return paginatedData;
+}
+
+// Columns
+const initialColumns = [
     {
-        key: 'companies',
-        $fetch: useNuxtApp().$api,
-        transform: (v) =>
-            v.map((company) => ({
+        key: 'name',
+        label: 'Name',
+        sortable: true,
+    },
+    {
+        key: 'industry',
+        label: 'Industry',
+        sortable: true,
+    },
+    {
+        key: 'size',
+        label: 'Size',
+        sortable: true,
+    },
+    {
+        key: 'location',
+        label: 'Location',
+        sortable: true,
+    },
+    {
+        key: 'website',
+        label: 'Website',
+        sortable: true,
+    },
+];
+const columns = [
+    ...initialColumns,
+    {
+        key: 'created_at',
+        label: 'Created At',
+        sortable: true,
+    },
+    {
+        key: 'email',
+        label: 'Email',
+        sortable: true,
+    },
+    {
+        key: 'linkedin',
+        label: 'LinkedIn',
+        sortable: true,
+    },
+    {
+        key: 'phone',
+        label: 'Phone',
+        sortable: true,
+    },
+    {
+        key: 'street',
+        label: 'Street',
+        sortable: true,
+    },
+    {
+        key: 'zip_code',
+        label: 'Zip Code',
+        sortable: true,
+    },
+];
+
+const selectedProvinces = ref([]);
+const selectedCities = ref([]);
+const selectedColumns = ref(initialColumns);
+const columnsTable = computed(() =>
+    columns.filter((column) => selectedColumns.value.includes(column))
+);
+
+const search = ref('');
+const resetFilters = () => {
+    search.value = '';
+};
+
+// Pagination
+const sort = ref({ column: 'id', direction: 'asc' as const });
+const page = ref(1);
+const pageCount = ref(10);
+const pageFrom = computed(() => (page.value - 1) * pageCount.value + 1);
+const pageTo = computed(() =>
+    Math.min(page.value * pageCount.value, pageTotal.value)
+);
+
+const { $api } = useNuxtApp();
+const [
+    { data: companies, pending },
+    { data: industries },
+    { data: sizes },
+    { data: provinces },
+    { data: cities },
+] = await Promise.all([
+    useLazyAsyncData(
+        'companies-paginated',
+        async () => {
+            const companies = await $api<IDataCompany[]>('/api/companies');
+            return companies.map((company) => ({
                 id: company.id,
                 name: company.name,
                 industry: company?.industry?.name ?? '',
@@ -16,104 +157,266 @@ const { data: companies, pending } = await useLazyFetch(
                 location: `${company?.city?.name}, ${company?.province?.name}`,
                 website: company.website,
                 avatar: company.avatar,
-            })),
-    }
-);
+                created_at: company.created_at,
+                email: company.email,
+                linkedin: company.linkedin,
+                phone: company.phone,
+                street: company.street,
+                zip_code: company.zip_code,
+            }));
+        },
+        {
+            default: () => [],
+            watch: [page, search, pageCount, sort],
+        }
+    ),
+    useAPI<Industry[]>('/api/industries', {
+        lazy: true,
+    }),
+    useAPI<Size[]>('/api/sizes', {
+        lazy: true,
+    }),
+    useAPI<Province[]>('/api/provinces', {
+        lazy: true,
+    }),
+    useAPI<City[]>('/api/cities', {
+        lazy: true,
+    }),
+]);
 
-const search = ref('');
-const debounceSearch = useDebounce(search, 300);
-const page = ref(1);
-const LIMIT = 10;
-const filteredRows = computed(() => {
-    if (!debounceSearch.value)
-        return companies.value?.slice(
-            (page.value - 1) * LIMIT,
-            page.value * LIMIT
-        );
+const pageTotal = ref(companies.value.length);
+const filteredCompanies = computed(() => {
+    if (!companies.value) return [];
 
-    // search
-    const searchedCompanies = companies.value?.filter((company) =>
-        company.name
-            .toLowerCase()
-            .includes(debounceSearch.value.toLowerCase().trim())
-    );
-
-    // pagination
-    return searchedCompanies?.slice(
-        (page.value - 1) * LIMIT,
-        page.value * LIMIT
-    );
+    return filterAndPaginate(companies.value, {
+        search: search.value,
+        page: page.value,
+        limit: pageCount.value,
+        sort: sort.value.column,
+        order: sort.value.direction,
+    });
 });
-
-const columns = [
-    {
-        label: 'Name',
-        key: 'name',
-    },
-    {
-        label: 'Industry',
-        key: 'industry',
-    },
-    {
-        label: 'Size',
-        key: 'size',
-    },
-    {
-        label: 'Location',
-        key: 'location',
-    },
-    {
-        label: 'Website',
-        key: 'website',
-    },
-];
 </script>
 
 <template>
-    <div class="p-3 bg-base-200 mt-3 flex justify-between items-center rounded">
-        <div class="flex items-center gap-2">
-            <p>Filter:</p>
-            <UPopover mode="hover">
-                <UButton
-                    variant="outline"
-                    color="gray"
-                    label="Open"
-                    class="focus:text-brand"
-                    >Industry</UButton
-                >
+    <!-- Filters -->
+    <div class="flex items-center justify-between gap-3 px-4 py-3"></div>
 
-                <!-- <template #panel>
-                    <div class="p-4">
-                        <Placeholder class="h-20 w-48" />
-                    </div>
-                </template> -->
-            </UPopover>
-            <UButton variant="outline" color="gray" class="focus:text-brand"
-                >Size</UButton
-            >
-            <UButton variant="outline" color="gray" class="focus:text-brand"
-                >Location</UButton
-            >
+    <!-- Header and Action buttons -->
+    <div class="p-3 bg-base-200 mt-3 flex justify-between items-center rounded">
+        <div class="flex items-center gap-4">
+            <UInput
+                v-model="search"
+                icon="i-heroicons-magnifying-glass-20-solid"
+                placeholder="Search..."
+            />
+
+            <div class="flex items-center gap-2">
+                <p>Filter:</p>
+                <!-- Industries Popover -->
+                <UPopover mode="hover">
+                    <UButton
+                        variant="outline"
+                        color="gray"
+                        class="hover:text-brand"
+                        size="xs"
+                        :ui="{
+                            rounded: 'rounded-xl',
+                            base: 'bg-base-100',
+                        }"
+                        >Industry</UButton
+                    >
+                    <template #panel>
+                        <div class="min-w-72">
+                            <p class="p-3 border-b font-semibold">
+                                Filter by Industry
+                            </p>
+                            <template v-if="industries">
+                                <div
+                                    class="p-3 bg-base-200 space-y-3 max-h-52 overflow-x-auto"
+                                >
+                                    <div
+                                        v-for="industry in industries"
+                                        :key="industry.id"
+                                        class="flex items-center gap-2"
+                                    >
+                                        <UCheckbox
+                                            :id="`industry-${industry.id}`"
+                                        />
+                                        <label
+                                            :for="`industry-${industry.id}`"
+                                            class="text-sm"
+                                        >
+                                            {{ industry.name }}
+                                        </label>
+                                    </div>
+                                </div>
+                                <div
+                                    class="p-3 flex justify-end gap-2 bg-base-200"
+                                >
+                                    <UButton variant="outline" size="sm"
+                                        >Cancel</UButton
+                                    >
+                                    <UButton size="sm">Apply</UButton>
+                                </div>
+                            </template>
+                            <template v-else>
+                                <p class="p-3 flex justify-center items-center">
+                                    no data
+                                </p>
+                            </template>
+                        </div>
+                    </template>
+                </UPopover>
+
+                <!-- Size Popover -->
+                <UPopover mode="hover">
+                    <UButton
+                        variant="outline"
+                        color="gray"
+                        class="hover:text-brand"
+                        size="xs"
+                        :ui="{
+                            rounded: 'rounded-xl',
+                            base: 'bg-base-100',
+                        }"
+                        >Size</UButton
+                    >
+                    <template #panel>
+                        <div class="min-w-72">
+                            <p class="p-3 border-b font-semibold">
+                                Filter by Company Size
+                            </p>
+                            <template v-if="sizes">
+                                <div
+                                    class="p-3 bg-base-200 space-y-3 max-h-52 overflow-x-auto"
+                                >
+                                    <div
+                                        v-for="size in sizes"
+                                        :key="size.id"
+                                        class="flex items-center gap-2"
+                                    >
+                                        <UCheckbox :id="`size-${size.id}`" />
+                                        <label
+                                            :for="`size-${size.id}`"
+                                            class="text-sm"
+                                        >
+                                            {{ size.size_range }}
+                                        </label>
+                                    </div>
+                                </div>
+                                <div
+                                    class="p-3 flex justify-end gap-2 bg-base-200"
+                                >
+                                    <UButton variant="outline" size="sm"
+                                        >Cancel</UButton
+                                    >
+                                    <UButton size="sm">Apply</UButton>
+                                </div>
+                            </template>
+                            <template v-else>
+                                <p class="p-3 flex justify-center items-center">
+                                    no data
+                                </p>
+                            </template>
+                        </div>
+                    </template>
+                </UPopover>
+
+                <UPopover mode="hover">
+                    <UButton
+                        variant="outline"
+                        color="gray"
+                        class="hover:text-brand"
+                        size="xs"
+                        :ui="{
+                            rounded: 'rounded-xl',
+                            base: 'bg-base-100',
+                        }"
+                        >Location</UButton
+                    >
+                    <template #panel>
+                        <div class="min-w-72">
+                            <p class="p-3 border-b font-semibold">
+                                Filter by Location
+                            </p>
+                            <div
+                                class="p-3 bg-base-200 space-y-3 max-h-52 overflow-x-auto"
+                            >
+                                <UFormGroup label="Province" name="province">
+                                    <UInputMenu
+                                        v-model="selectedProvinces"
+                                        :options="
+                                            provinces?.map(
+                                                ({ name }) => name ?? []
+                                            )
+                                        "
+                                    />
+                                </UFormGroup>
+
+                                <UFormGroup label="City" name="city">
+                                    <UInputMenu
+                                        v-model="selectedCities"
+                                        :options="
+                                            cities?.map(
+                                                ({ name }) => name ?? []
+                                            )
+                                        "
+                                    />
+                                </UFormGroup>
+                            </div>
+                            <div class="p-3 flex justify-end gap-2 bg-base-200">
+                                <UButton variant="outline" size="sm"
+                                    >Cancel</UButton
+                                >
+                                <UButton size="sm">Apply</UButton>
+                            </div>
+                        </div>
+                    </template>
+                </UPopover>
+            </div>
         </div>
 
-        <UInput
-            v-model="search"
-            placeholder="Search"
-            class="ml-2"
-            icon="i-heroicons-magnifying-glass"
-        />
+        <div class="flex gap-1.5 items-center">
+            <!-- Columns Selector -->
+            <USelectMenu
+                v-model="selectedColumns"
+                :options="columns"
+                multiple
+                :uiMenu="{ width: 'min-w-32' }"
+            >
+                <UButton icon="i-heroicons-view-columns" color="gray" size="xs">
+                    Columns
+                </UButton>
+            </USelectMenu>
+
+            <!-- Reset Filters Button -->
+            <UButton
+                icon="i-heroicons-funnel"
+                color="gray"
+                size="xs"
+                :disabled="search === ''"
+                @click="resetFilters"
+            >
+                Reset
+            </UButton>
+        </div>
     </div>
 
+    <!-- Table -->
     <UTable
-        :rows="filteredRows ?? []"
-        :columns="columns"
-        class="mt-5"
+        v-model:sort="sort"
+        :rows="filteredCompanies"
+        :columns="columnsTable"
         :loading="pending"
-        :loading-state="{
-            icon: 'i-heroicons-arrow-path-20-solid',
-            label: 'Loading...',
+        sort-asc-icon="i-heroicons-arrow-up"
+        sort-desc-icon="i-heroicons-arrow-down"
+        sort-mode="manual"
+        class="w-full"
+        :ui="{
+            td: { base: 'max-w-[0] truncate' },
+            default: { checkbox: { color: 'gray' } },
         }"
-        :progress="{ color: 'primary', animation: 'carousel' }"
     >
         <template #name-data="{ row }">
             <div class="flex items-center gap-2">
@@ -141,6 +444,26 @@ const columns = [
             </NuxtLink>
         </template>
 
+        <template #linkedin-data="{ row }">
+            <NuxtLink
+                :href="row.linkedin"
+                class="text-brand hover:underline"
+                external
+                target="_blank"
+            >
+                {{ row.linkedin }}
+            </NuxtLink>
+        </template>
+
+        <template #created_at-data="{ row }">
+            {{
+                useDateFormat(
+                    row.created_at,
+                    'YYYY-MM-DD HH:mm:ss'
+                ).value.replace('"', '')
+            }}
+        </template>
+
         <template #empty-state>
             <div
                 class="flex flex-col items-center justify-center py-10 gap-y-5"
@@ -156,11 +479,45 @@ const columns = [
         </template>
     </UTable>
 
-    <div v-if="filteredRows?.length" class="flex justify-end mt-4">
+    <!-- Number of rows & Pagination -->
+
+    <div class="flex flex-wrap justify-between items-center mt-5">
+        <div>
+            <span class="text-sm leading-5">
+                Showing
+                <span class="font-medium">{{ pageFrom }}</span>
+                to
+                <span class="font-medium">{{ pageTo }}</span>
+                of
+                <span class="font-medium">{{ pageTotal }}</span>
+                results
+            </span>
+        </div>
+
+        <div class="flex items-center gap-1.5">
+            <span class="text-sm leading-5">Rows per page:</span>
+
+            <USelect
+                v-model="pageCount"
+                :options="[3, 5, 10, 20, 30, 40]"
+                class="me-2 w-20"
+                size="xs"
+            />
+        </div>
+
         <UPagination
             v-model="page"
-            :page-count="LIMIT"
-            :total="companies?.length ?? 0"
+            :page-count="pageCount"
+            :total="pageTotal"
+            :ui="{
+                wrapper: 'flex items-center gap-1',
+                rounded: '!rounded-full min-w-[32px] justify-center',
+                default: {
+                    activeButton: {
+                        variant: 'outline',
+                    },
+                },
+            }"
         />
     </div>
 </template>
