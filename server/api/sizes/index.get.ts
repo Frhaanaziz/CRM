7db4830ca069cf5605a55f5d0ca041f12a474788
Farhan <farhan@aththariq.com>
2024-06-12 17:33:34 +0700

    feat: Add API endpoints for fetching cities, industries, provinces, and sizes

---
+++ server/api/sizes/index.get.ts
@@ -0,0 +1,21 @@
+import { H3Event } from 'h3';
+import type { Database } from '~/types/supabase';
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event: H3Event) => {
+    const supabase = await serverSupabaseClient<Database>(event);
+
+    const res = await supabase
+        .from('Sizes')
+        .select()
+        .order('created_at', { ascending: false });
+    if (res.error) {
+        console.error('Error fetching sizes', res.error);
+        throw createError({
+            status: 500,
+            statusMessage: res.error.message,
+        });
+    }
+
+    return res.data;
+});
