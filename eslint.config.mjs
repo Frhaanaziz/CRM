eea9f6c25f63259961b372224fd9d2657da4f879
Farhan <farhan@aththariq.com>
2024-06-12 17:32:42 +0700

    chore: Update eslint.config.mjs with additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,16 @@
 import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
-  rules: {
-    'vue/first-attribute-linebreak': 'off',
-    '@typescript-eslint/consistent-type-imports': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    'vue/html-self-closing': 'off',
-  },
+    rules: {
+        'vue/first-attribute-linebreak': 'off',
+        '@typescript-eslint/consistent-type-imports': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/attribute-hyphenation': 'off',
+    },
 });
 // Your custom configs here
