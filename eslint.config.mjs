9ebbade1ebba490f6273d0460947b4a3bc93ead0
Farhan <farhan@aththariq.com>
2024-06-13 15:05:41 +0700

    chore: Update prettier and eslint dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt({
-    rules: {
-        // 'vue/first-attribute-linebreak': 'off',
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            { argsIgnorePattern: '^_' },
-        ],
-        '@typescript-eslint/no-explicit-any': 'off',
-        'vue/html-self-closing': 'off',
-        'vue/attribute-hyphenation': 'off',
-        'prefer-const': 'warn',
+export default withNuxt([
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...eslintConfigPrettier.rules,
+
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'off',
+            'vue/html-self-closing': 'off',
+            'vue/attribute-hyphenation': 'off',
+            'prefer-const': 'warn',
+        },
     },
-});
-// Your custom configs here
+]);
