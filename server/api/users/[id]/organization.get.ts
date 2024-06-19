0656f23315ceb8dd4def8ec59bc79a9a41a54a9f
Farhan <farhan@aththariq.com>
2024-06-19 13:59:10 +0700

    feat: Add get organization by id endpoint

---
+++ server/api/users/[id]/organization.get.ts
@@ -0,0 +1,24 @@
+import type { H3Event } from 'h3';
+import { serverSupabaseClient } from '#supabase/server';
+import type { Database } from '~/types/supabase';
+
+export default defineEventHandler(async (event: H3Event) => {
+    const supabase = await serverSupabaseClient<Database>(event);
+
+    // get user id from params
+    const user_id = event.context.params?.id;
+    if (!user_id) throw createError({ status: 400, statusMessage: 'User id is needed' });
+
+    const { data: user, error } = await supabase
+        .from('Users')
+        .select(
+            `
+            organization: Organizations(*)
+            `,
+        )
+        .eq('id', user_id)
+        .single();
+    if (error) throw createError({ status: 500, statusMessage: error.message });
+
+    return user.organization;
+});
