5f8b3ecb40045b9167be4ab5496abd161fa91e29
Farhan <farhan@aththariq.com>
2024-06-20 17:46:06 +0700

    feat: Add endpoint to fetch organization users

---
+++ server/api/organizations/[id]/users.get.ts
@@ -0,0 +1,26 @@
+import type { H3Event } from 'h3';
+import type { Database } from '~/types/supabase';
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event: H3Event) => {
+    const supabase = await serverSupabaseClient<Database>(event);
+
+    const res = await supabase
+        .from('Users')
+        .select(
+            `
+            *,
+            role: Roles(name)
+            `,
+        )
+        .order('created_at', { ascending: false });
+    if (res.error) {
+        console.error('Error fetching organization users', res.error);
+        throw createError({
+            status: 500,
+            statusMessage: res.error.message,
+        });
+    }
+
+    return res.data;
+});
