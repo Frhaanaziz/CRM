ef91718ef5f64f2f9559e7c948fd3f032cee9286
Farhan <farhan@aththariq.com>
2024-06-24 12:24:11 +0700

    refactor: Update user role and status in ModalUpdateUserOrganization.vue

--- components/modal/ModalUpdateUserOrganization.vue
+++ components/modal/ModalUpdateUserOrganization.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import type { z } from 'zod';
 import type { FormSubmitEvent } from '#ui/types';
-import type { Role, User } from '~/types';
+import type { User } from '~/types';
 
 const props = defineProps<{
-    user: { id: User['id']; role: Role['name'] };
+    user: { id: User['id']; role_id: User['role_id']; status: User['status'] };
 }>();
 const emit = defineEmits(['close']);
 
@@ -13,22 +13,33 @@ function closeModal() {
 }
 
 const { data: roles } = useLazyFetch('/api/roles', { key: 'roles' });
-const rolesOption = computed(() => roles.value?.map((role) => ({ value: role.name, label: capitalize(role.name) })));
+const rolesOption = computed(() => roles.value?.map((role) => ({ value: role.id, label: capitalize(role.name) })));
+const statusOption = ['active', 'inactive'].map((status) => ({ value: status, label: capitalize(status) }));
 
-type UpdateUserOrganizationType = z.infer<typeof updateUserOrganizationSchema>;
+const schema = userSchema.pick({ id: true, role_id: true, status: true });
+type UpdateUserOrganizationType = z.infer<typeof schema>;
 
 const isSubmitting = ref(false);
 const state = ref({
-    role: props.user.role,
+    id: props.user.id,
+    role_id: props.user.role_id,
+    status: props.user.status,
 });
 async function handleSubmit(event: FormSubmitEvent<UpdateUserOrganizationType>) {
     try {
         isSubmitting.value = true;
 
-        await $fetch(`/api/users/${props.user.id}/role`, {
-            method: 'PATCH',
-            body: JSON.stringify(event.data),
-        });
+        const { id, role_id, status } = event.data;
+        await Promise.all([
+            $fetch(`/api/users/${props.user.id}/role`, {
+                method: 'PATCH',
+                body: JSON.stringify({ id, role_id }),
+            }),
+            $fetch(`/api/users/${props.user.id}/status`, {
+                method: 'PATCH',
+                body: JSON.stringify({ id, status }),
+            }),
+        ]);
 
         closeModal();
         toast.success('Member updated successfully');
@@ -54,22 +65,27 @@ async function handleSubmit(event: FormSubmitEvent<UpdateUserOrganizationType>) 
         </div>
 
         <div class="space-y-3 bg-base-200 p-3">
-            <UForm
-                :schema="updateUserOrganizationSchema"
-                :state="state"
-                class="space-y-3"
-                @submit="handleSubmit"
-                @error="console.error"
-            >
+            <UForm :schema="schema" :state="state" class="space-y-3" @submit="handleSubmit" @error="console.error">
                 <UFormGroup label="Role" name="role" required :ui="{ label: { base: 'font-semibold' } }">
                     <USelectMenu
-                        v-model="state.role"
+                        v-model="state.role_id"
                         value-attribute="value"
                         option-attribute="label"
                         :options="rolesOption"
                         :loading="isSubmitting"
                         :disabled="isSubmitting"
                         placeholder="Select role"
+                    />
+                </UFormGroup>
+                <UFormGroup label="Status" name="status" required :ui="{ label: { base: 'font-semibold' } }">
+                    <USelectMenu
+                        v-model="state.status"
+                        value-attribute="value"
+                        option-attribute="label"
+                        :options="statusOption"
+                        :loading="isSubmitting"
+                        :disabled="isSubmitting"
+                        placeholder="Select status"
                     />
                 </UFormGroup>
 
