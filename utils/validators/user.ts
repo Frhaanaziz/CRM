044bc5bb9696b78ac567ad8d4dbfcdf71b0d456a
Farhan <farhan@aththariq.com>
2024-06-24 12:19:30 +0700

    refactor: Remove profile validator and add user validator

---
+++ utils/validators/user.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { phone } from '.';
+import { userStatuses } from '../constants';
+
+export const userSchema = z.object({
+    id: z.string(),
+    created_at: z.coerce.date(),
+    email: z.string().email(),
+    first_name: z.string().min(1, { message: 'First name is required' }),
+    last_name: z.string().min(1, { message: 'Last name is required' }),
+    linkedin: z.string().nullable().optional(),
+    phone: phone(z.string()),
+    photo: z.string().url().nullable().optional(),
+    status: z.enum(userStatuses, {
+        message: "Invalid status. Must be 'active' or 'inactive'",
+    }),
+    role_id: z.number().int(),
+    updated_at: z.coerce.date(),
+});
+
+export const updateUserSchema = userSchema.pick({ first_name: true, last_name: true, phone: true, linkedin: true }).partial();
+export const updateUserRoleSchema = userSchema.pick({ id: true, role_id: true });
+export const updateUserStatusSchema = userSchema.pick({ id: true, status: true });
