535fa9640180cfcadc8aebfe50f445cf4bc0e8bb
Farhan <farhan@aththariq.com>
2024-07-25 17:23:18 +0700

    feat: Add similar companies feature to b2b company details

--- server/api/b2b-companies/[id]/index.get.ts
+++ server/api/b2b-companies/[id]/index.get.ts
@@ -1,38 +1,8 @@
-import { serverSupabaseClient } from '#supabase/server';
-import type { Database } from '~/types/supabase';
-
 export default defineEventHandler(async (event) => {
-    const supabase = await serverSupabaseClient<Database>(event);
-
     const id = event.context.params!.id;
 
-    const [companyRes, contactsRes] = await Promise.all([
-        supabase
-            .from('B2B_Companies')
-            .select(
-                `
-            *,
-            industry: Industries(*),
-            size: Sizes(*),
-            province: Provinces(*),
-            city: Cities(*)
-            `
-            )
-            .eq('id', id)
-            .single(),
-        supabase.from('B2B_Contacts').select('*').eq('company_id', id).order('created_at', { ascending: false }),
-    ]);
-    if (companyRes.error) {
-        console.error('Error getting lead:', companyRes.error);
-        throw createError({ status: 400, statusMessage: companyRes.error.message });
-    }
-    if (contactsRes.error) {
-        console.error('Error getting tasks:', contactsRes.error);
-        throw createError({ status: 400, statusMessage: contactsRes.error.message });
-    }
+    const fetchApi = await backendApi(event);
+    const { data } = await fetchApi<{ data: any }>(`/b2b-companies/${id}`);
 
-    const { data: companyData } = companyRes;
-    const { data: contactsData } = contactsRes;
-
-    return { ...companyData, b2b_contacts: contactsData };
+    return data;
 });
