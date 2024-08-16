66cceca79acd760d3f1e6d7b6416ab4c64f3e84c
Farhan <farhan@aththariq.com>
2024-08-16 13:10:11 +0700

    refactor: Improve authentication middleware logic

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,16 +1,21 @@
 export default defineNuxtRouteMiddleware((to) => {
-    if (to.path.startsWith('/auth')) return;
+    const needcheck = ['/auth/onboarding'];
+    if (to.path.startsWith('/auth') && !needcheck.includes(to.path)) return;
 
     if (import.meta.server) {
         const session = useSupabaseSession();
         if (!session.value) return navigateTo('/auth/signin');
         if (!session.value.user.user_metadata.organization_id) return navigateTo('/auth/onboarding');
+
+        if (to.path === '/auth/onboarding' && session.value.user.user_metadata.organization_id) return navigateTo('/dashboard');
     }
 
     // Use pinia store in client because supabase session is not updated in the client if refreshSession() is executed
     if (import.meta.client) {
         const { user, session } = storeToRefs(userSessionStore());
         if (!session.value) return navigateTo('/auth/signin');
-        if (!user.value || !user.value.user_metadata?.organization_id) return navigateTo('/auth/onboarding');
+        if (!user.value?.user_metadata?.organization_id) return navigateTo('/auth/onboarding');
+
+        if (to.path === '/auth/onboarding' && session.value.user.user_metadata.organization_id) return navigateTo('/dashboard');
     }
 });
