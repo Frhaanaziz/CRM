0602501055a1ec7e4b89291493d4ab77c52399ac
Farhan <farhan@aththariq.com>
2024-08-22 12:28:29 +0700

    refactor: Update authentication middleware to handle organization onboarding flow

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -5,17 +5,19 @@ export default defineNuxtRouteMiddleware((to) => {
     if (import.meta.server) {
         const session = useSupabaseSession();
         if (!session.value) return navigateTo('/auth/signin');
-        if (!session.value.user.user_metadata.organization_id) return navigateTo('/auth/onboarding');
-
-        if (to.path === '/auth/onboarding' && session.value.user.user_metadata.organization_id) return navigateTo('/dashboard');
+        else if (to.path !== '/auth/onboarding' && !session.value.user.user_metadata.organization_id)
+            return navigateTo('/auth/onboarding');
+        else if (to.path === '/auth/onboarding' && session.value.user.user_metadata.organization_id)
+            return navigateTo('/dashboard');
     }
 
     // Use pinia store in client because supabase session is not updated in the client if refreshSession() is executed
     if (import.meta.client) {
-        const { user, session } = storeToRefs(userSessionStore());
+        const { session } = storeToRefs(userSessionStore());
         if (!session.value) return navigateTo('/auth/signin');
-        if (!user.value?.user_metadata?.organization_id) return navigateTo('/auth/onboarding');
-
-        if (to.path === '/auth/onboarding' && session.value.user.user_metadata.organization_id) return navigateTo('/dashboard');
+        else if (to.path !== '/auth/onboarding' && !session.value?.user.user_metadata.organization_id)
+            return navigateTo('/auth/onboarding');
+        else if (to.path === '/auth/onboarding' && session.value?.user.user_metadata.organization_id)
+            return navigateTo('/dashboard');
     }
 });
